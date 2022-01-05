24b4aa0a825df6e7f9202f8f4c028c1effcbff48
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-01-05 13:19:42 +0900

    #3 External Middleware (morgan)

--- src/server.js
+++ src/server.js
@@ -1,43 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 // create application 
 const app = express();
+const logger = morgan("dev");
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-}
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if(url === "/protected"){
-    return res.send("<h1>Not Allowed</h1>")
-  }
-  console.log("Allowed, you may continue.")
-  next();
-}
-
-const handleHome = (req, res) => {
+const Home = (req, res) => {
   return res.send("<h1>I still love you!</h1>");
 };
 
-const handleLogin =(req, res) => {
-  return res.send({ message: "Login here."});
-}
-
-const handleProtected = (req, res) =>{
-  return res.send("Welcome to the private lounge");
-}
-
-app.use(gossipMiddleware);
-app.use(privateMiddleware);
-app.get("/", handleHome); 
-app.get("/protected", handleProtected); 
-
-app.get("/login", handleLogin); 
-
+app.use(logger);
+app.get("/", Home); 
 
 // listen to external connection (외부접속)
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
