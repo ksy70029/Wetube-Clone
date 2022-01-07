8535c68b6d4319d91dd70c8318b658126fd8a2ce
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-01-07 09:41:05 +0900

    #4 Routers

---
+++ README.md
@@ -0,0 +1,21 @@
+# Wetube Clone
+
+<!-- global router -->
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+<!-- users router -->
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+<!-- video router -->
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Delete Video
+/videos/comments/delete -> Delete A Commet on a video
--- src/server.js
+++ src/server.js
@@ -6,13 +6,30 @@ const PORT = 4000;
 // create application 
 const app = express();
 const logger = morgan("dev");
-
-const Home = (req, res) => {
-  return res.send("<h1>I still love you!</h1>");
-};
-
 app.use(logger);
-app.get("/", Home); 
+
+//create global router
+const globalRouter = express.Router();
+  //create controller
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+//create users router
+const userRouter = express.Router();
+  //create controller
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+//create videos router
+const videoRouter = express.Router();
+  //create controller
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+videoRouter.get("/watch", handleWatchVideo);
+
+//create application to start server and to use routers
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 // listen to external connection (외부접속)
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
