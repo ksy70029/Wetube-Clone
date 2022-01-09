7f47ef3f461dc835f4fc1abbf751b22b90c2f55d
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-01-09 22:54:43 +0900

    #5.7 Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,11 @@
-export const trending = (req, res) => res.render("home", {pageTitle:"Home"});
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: true,
+};
+
+export const trending = (req, res) => res.render("home", {pageTitle:"Home", fakeUser: fakeUser});
+export const see = (req, res) => res.render("watch", {pageTitle:"Watch"});
+export const edit = (req, res) => res.render("edit", {pageTitle:"Edit"});
 export const search = (req, res) => res.send("Search Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const upload = (req, res) => res.send("Upload Video");
