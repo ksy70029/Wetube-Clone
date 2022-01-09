a10ddb1aff6803ab8347e8d6babcbe96da2db6a3
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-01-09 17:27:10 +0900

    #5.4 Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,6 @@
 export const trending = (req, res) => res.render("home");
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-  console.log(req.params);
-  res.send("Edit Video");
-}
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Video");
 export const deleteVideo = (req, res) => res.send("Delete Video");
 export const upload = (req, res) => res.send("Upload Video");
