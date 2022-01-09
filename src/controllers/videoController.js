cf82be2e68508dc1158bfe82b67269a9d4374a1f
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-01-09 16:28:07 +0900

    #5.2 Partials

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,5 @@
 export const trending = (req, res) => res.render("home");
-export const see = (req, res) => {
-  res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>See video</h1></body></html>`);
-}
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => {
   console.log(req.params);
   res.send("Edit Video");
