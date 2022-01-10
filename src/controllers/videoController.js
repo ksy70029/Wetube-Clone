19768d3035ee0b20015ef569ac28b2be6b1dab56
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-01-11 01:30:13 +0900

    #6.1 Array Database part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ const videos = [
     rating: 5,
     comments:2,
     createdAt: "2 minutes ago",
-    views: 59,
+    views: 1,
     id:1,
   },
   {
@@ -30,7 +30,7 @@ export const trending = (req, res) => {
 export const see = (req, res) => {
   const {id} = req.params;
   const video = videos[id-1];
-  return res.render("watch", {pageTitle:`Watching ${video.title}`});
+  return res.render("watch", {pageTitle:`Watching ${video.title}`, video});
 };
 export const edit = (req, res) => res.render("edit", {pageTitle:"Edit"});
 export const search = (req, res) => res.send("Search Video");
