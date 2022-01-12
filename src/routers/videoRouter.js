07ca30a7f42e68eee00bb7a3cf52abb465f3c4bc
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-01-12 13:43:40 +0900

    #6.5, #6.6 More Practice

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { see, getEdit, postEdit } from "../controllers/videoController";
+import { see, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", see);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 
 export default videoRouter;
