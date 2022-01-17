72dabe845efd8b516f7d7fe23a4cc2a92ff2e178
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-01-17 19:13:23 +0900

    #6.25 Delete Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { see, getEdit, postEdit, getUpload, postUpload } from "../controllers/videoController";
+import { see, getEdit, postEdit, getUpload, postUpload, deleteVideo} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})", see);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-
 
 export default videoRouter;
