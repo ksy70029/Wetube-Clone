77367d44a1f12cf2bec477e9453ed275169f1722
ksy70029 <80887385+ksy70029@users.noreply.github.com>
2022-03-16 16:28:58 +0900

    #7.21 Github Login part Six, #7.22 Log Out

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -37,7 +37,7 @@ export const getLogin = (req, res) => res.render("login", {pageTitle:"Login"});
 export const postLogin = async(req, res) => {
   const {username, password} = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({username});
+  const user = await User.findOne({username, socialOnly:false});
   if(!user){
     return res
     .status(400)
@@ -99,18 +99,35 @@ export const finishGithubLogin = async (req, res) =>{
         Authorization: `token ${access_token}`,
       },
     })).json();
-    const email = emailData.find(
+    const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
-    if (!email) {
+    if (!emailObj) {
       return res.redirect("/login");
     }
+    let user = await User.findOne({email: emailObj.email});
+    if(!user){
+      user = await User.create({
+        avatarUrl:userData.avatar_url,
+        name:userData.name,
+        username:userData.login,
+        email:emailObj.email,
+        password:"",
+        socialOnly:true,
+        location:userData.location,
+      });
+    }
+      req.session.loggedIn = true;
+      req.session.user = user;
+      return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
 };
 
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Log Out");
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const see = (req, res) => res.send("See User");
